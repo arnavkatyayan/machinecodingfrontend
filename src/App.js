9ea83705318265cdc3bf2771e0036ec7b5d0d0da
Arnav Katyayan <ishumisra99@gmail.com>
2025-12-18 23:29:55 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          Machine coding
         </a>
       </header>
     </div>
